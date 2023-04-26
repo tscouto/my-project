9efb4698549fd7b62ba5b86299ceaca62caa18b9
tscouto <tiago.dscouto@hotmail.com>
2023-04-25 22:19:56 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+
 import './App.css';
+import HelloWolrd from './components/HelloWolrd'
+import SayMyName from './components/SayMyName';
+import Pessoa from './components/Pessoa';
+import List from './components/List';
+import Frase from './components/Frase';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const nome = "Maria"
+
+    return (
+        <div className='App'>
+            <Frase />
+            <Frase />
+            <SayMyName nome='Tiago' />
+            <SayMyName nome='João' />
+            <SayMyName nome={nome} />
+            <Pessoa nome='Tiago' idade='33' profissao='Engenheiro de Software' foto='https://via.placeholder.com/150' />
+            <List/>
+
+
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/SayMyName.js
@@ -0,0 +1,8 @@
+function SayMyName(props) {
+    return(
+        <div>
+            <p>Fala ai {props.nome}, suave?</p>
+        </div>
+    )
+}
+export default SayMyName
