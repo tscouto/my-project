8cdfc900e0dec191634221df3f8a16582972042a
tscouto <tiago.dscouto@hotmail.com>
2023-04-25 23:47:59 -0300

    update

--- src/App.js
+++ src/App.js
@@ -1,28 +1,32 @@
 
 import './App.css';
-import HelloWolrd from './components/HelloWolrd'
-import SayMyName from './components/SayMyName';
-import Pessoa from './components/Pessoa';
-import List from './components/List';
-import Frase from './components/Frase';
+import Evento  from './components/Evento';
+// import HelloWolrd from './components/HelloWolrd'
+// import SayMyName from './components/SayMyName';
+// import Pessoa from './components/Pessoa';
+// import List from './components/List';
+// import Frase from './components/Frase';
 
 function App() {
 
-    const nome = "Maria"
-
+    
     return (
         <div className='App'>
-            <Frase />
-            <Frase />
-            <SayMyName nome='Tiago' />
-            <SayMyName nome='João' />
-            <SayMyName nome={nome} />
-            <Pessoa nome='Tiago' idade='33' profissao='Engenheiro de Software' foto='https://via.placeholder.com/150' />
-            <List/>
-
-
+            <Evento numero="1"/>
+            <Evento numero="2"/>
         </div>
     );
 }
 
 export default App;
+
+
+// {/* <Frase /> */}
+            // <Frase />
+            // <SayMyName nome='Tiago' />
+            // <SayMyName nome='João' />
+            // const nome = "Maria"
+
+            // <SayMyName nome={nome} />
+            // <Pessoa nome='Tiago' idade='33' profissao='Engenheiro de Software' foto='https://via.placeholder.com/150' />
+            // <List/>
---
+++ src/components/Form.js
@@ -0,0 +1,16 @@
+function Form() {
+    return(
+        <div>
+            <h1>Meu cadastro</h1>
+            <form onSubmit={cadastrarUsuario} >
+                <div>
+                    <input type="text"placeholder="Digite o seu nome" />
+                </div>
+                <div>
+                    <input type="submit" value="Cadastrar"/>
+                </div>
+            </form>
+        </div>
+    )
+}
+export default Form
