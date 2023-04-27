2ed0c480124b25b3382af5282ceab84b49607e43
tscouto <tiago.dscouto@hotmail.com>
2023-04-27 14:04:20 -0300

    update

--- src/components/Condicional.js
+++ src/components/Condicional.js
@@ -12,12 +12,22 @@ function Condicional() {
         setUserEmail(email)
         console.log(userEmail)
     }
+
+    function limparEmail() {
+        setUserEmail('')
+    }
     return (
         <div>
             <h2>Cadastre o seu e-mail</h2>
             <form action="">
                 <input type="email" placeholder='Digite o seu e-mail...' onChange={(e)=> setEmail(e.target.value)} />
                 <button type='submit' onClick={enviarEmail}>Enviar-email</button>
+                {userEmail && (
+                    <div>
+                        <p>O e-mail do usuario é:{userEmail}</p>
+                    <button onClick={limparEmail}>Limpar </button>
+                    </div>
+                )}
             </form>
 
         </div>
