6c95b4efcf92cbfe35c3929cae74ec00a7f87c2c
tscouto <tiago.dscouto@hotmail.com>
2023-04-29 10:53:41 -0300

    uptate

--- src/components/Condicional.js
+++ src/components/Condicional.js
@@ -30,18 +30,11 @@ function Condicional() {
                 <button type='submit' onClick={enviarEmail}>Enviar-email</button>
                 {userEmail && (
                     <div>
-<<<<<<< HEAD
                        <p>O e-mail do usuario e:{userEmail}</p>
                        <button onClick={limparEmail}>Limpar e-mail</button>
                     </div>
                 )}
 
-=======
-                        <p>O e-mail do usuario é:{userEmail}</p>
-                    <button onClick={limparEmail}>Limpar </button>
-                    </div>
-                )}
->>>>>>> 2ed0c480124b25b3382af5282ceab84b49607e43
             </form>
 
         </div>
